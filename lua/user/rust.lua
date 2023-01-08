f9a4d2b7cb825fb81fb6c0aceb309844c83defb2
Bogdan Pop <pop.bogdan.pvab@gmail.com>
2023-01-08 19:24:35 +0100

    feat(lsp): Fix on_attach behaviour for rust-tools

--- lua/user/rust.lua
+++ lua/user/rust.lua
@@ -4,15 +4,18 @@ if not rust_status_ok then
 end
 
 -- Update this path
-local extension_path = vim.env.HOME .. '/.vscode/extensions/vadimcn.vscode-lldb-1.8.1/'
-local codelldb_path = extension_path .. 'adapter/codelldb'
-local liblldb_path = extension_path .. 'lldb/lib/liblldb.so'
+local extension_path = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.8.1/"
+local codelldb_path = extension_path .. "adapter/codelldb"
+local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
 
 local opts = {
-    -- ... other configs
-    dap = {
-        adapter = require('rust-tools.dap').get_codelldb_adapter(
-            codelldb_path, liblldb_path)
-    }
+	-- ... other configs
+	dap = {
+		adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+	},
+	server = {
+		on_attach = require("user.lsp.handlers").on_attach,
+		capabilities = require("user.lsp.handlers").capabilities,
+	},
 }
 rust_tools.setup(opts)
