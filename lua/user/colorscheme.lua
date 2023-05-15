f6843f2f906ea145d8a5aed296f178e75474a4b3
Bogdan Pop <pop.bogdan.pvab@gmail.com>
2023-05-16 00:34:54 +0200

    fix(highlight): Disable lsp highlight

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -2,5 +2,13 @@ local colorscheme = "darkplus"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  return
+	return
+end
+
+-- Hide semantic highlights for functions
+vim.api.nvim_set_hl(0, "@lsp.type.function", {})
+
+-- Hide all semantic highlights
+for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+	vim.api.nvim_set_hl(0, group, {})
 end
